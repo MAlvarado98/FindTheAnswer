3b56526ffce43ef5cff3e946e4ca55d96c42b2dc
Mario Alvarado <marioalvarado@Marios-Mac.local>
2020-06-14 17:07:38 -0700

    First commit + .gitignore added + README.md added + Project base added

---
+++ FindTheAnswer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FindTheAnswer
+//
+//  Created by Mario Alvarado on 6/14/20.
+//  Copyright © 2020 Mario Alvarado. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
