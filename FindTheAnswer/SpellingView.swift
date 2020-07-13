a06cd3cd8ceccc79fff4792ced01aa4bf9077b47
Mario Alvarado <marioalvarado@Marios-Mac.local>
2020-07-13 12:00:46 -0700

    Spelling works + Spelling game works - Spelling game needs design

--- FindTheAnswer/SpellingView.swift
+++ FindTheAnswer/SpellingView.swift
@@ -8,8 +8,32 @@
 
 import SwiftUI
 
+struct Response: Codable{
+    var id: Int
+    var word: String
+}
+/*
+class Options: ObservableObject{
+    @Published var options: [Character]
+    init(){
+        self.options = []
+    }
+}*/
+
 struct SpellingView: View {
+    //@ObservedObject var myOptions: Options = Options()
     @ObservedObject var viewRouter: ViewRouter
+    
+    @State private var word: String = ""
+    @State private var pos: Int = 0
+    @State private var character: Character = "a"
+    @State private var options: [Character] = ["a","a","a","a"]
+    @State private var alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+    @State private var showMessage: Bool = false
+    @State private var activeAlert: Int = 0 //0 is correct, 1 is error, 2 lost
+    @State private var score: Int = 0
+    @State private var lifes: Int = 3
+    
     var body: some View {
         VStack{
             Spacer()
@@ -20,29 +44,107 @@ struct SpellingView: View {
                     Text("↩︎ Go Back")
                 }
                 Spacer()
-                Text("Score: ")
+                Text("Score: \(self.score)")
                 Spacer()
-                Text("Lifes: ")
+                Text("Lifes: \(self.lifes)")
                 Spacer()
             }
             Spacer()
-            Text("RANDOM_WORD")
+            Text("\(self.word)")
             Spacer()
             HStack{
                 Spacer()
                 Text("Answer")
                 Spacer()
-                ForEach (1 ..< 5){number in
+                ForEach ( 0 ..< self.options.count ){ value in
                     Button(action: {
-                       
+                        if self.options[value] == self.character{
+                            self.activeAlert = 0
+                            self.showMessage = true
+                            self.score += 1
+                            self.getWord()
+                        }else{
+                            if self.lifes > 0{
+                                self.activeAlert = 1
+                                self.showMessage = true
+                                self.lifes -= 1
+                                self.getWord()
+                            }else{
+                                self.activeAlert = 2
+                                self.showMessage = true
+                                DispatchQueue.main.asyncAfter(deadline: .now()+3){
+                                    self.viewRouter.currentPage = "home"
+                                }
+                            }
+                            
+                        }
                     }){
-                        Text("A")
+                        Text("\(String(self.options[value]))")
+                    }
+                    .alert(isPresented: self.$showMessage){
+                        switch self.activeAlert{
+                        case 0:
+                            return Alert(title: Text("✔︎"))
+                        case 1:
+                            return Alert(title: Text("✖︎"))
+                        case 2:
+                            return Alert(title: Text("✖︎"), message: Text("Sorry, you've lost \nYou're score was \(self.score)"))
+                        default:
+                            return Alert(title: Text("Default"))
+                        }
                     }
                     Spacer()
                 }
             }
             Spacer()
+        }.onAppear(perform: getWord)
+    }
+    
+    func getWord() {
+        guard let url = URL(string: "http://api.wordnik.com/v4/words.json/randomWords?hasDictionaryDef=true&minCorpusCount=0&minLength=5&maxLength=15&limit=1&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5") else {
+            print("Invalid URL")
+            return
         }
+        
+        let request = URLRequest(url: url)
+        
+        URLSession.shared.dataTask(with: request){data, response, error in
+            if let data = data {
+                /*if let dataString = String(data: data, encoding: .utf8){
+                    print("Response \(dataString)")
+                    return
+                }*/
+                do {
+                    let returnValue = try JSONDecoder().decode([Response].self, from: data)
+                    let nWord = returnValue[0].word.uppercased()
+                    print(nWord)
+                    self.pos = Int.random(in: 0..<String(nWord).count)
+                    self.character = nWord[nWord.index(nWord.startIndex, offsetBy: self.pos)]
+                    self.word = String(nWord.dropLast((nWord.count)-self.pos))+"?"+String(nWord.dropFirst(self.pos+1))
+                    //Push options
+                    let randPos = Int.random(in: 0..<4)
+                    self.options[randPos] = self.character
+                    var alph = self.alphabet
+                    if self.character != "-"{
+                        alph.remove(at:
+                            alph.firstIndex(of: String(self.character))!)
+                    }
+                    for n in 0..<4 {
+                        if(n != randPos){
+                            let rand = Int.random(in: 0 ..< alph.count)
+                            self.options[n] = Character(alph[rand])
+                            alph.remove(at: rand)
+                        }
+                    }
+                    return
+                }catch{
+                    print("Error")
+                }
+                
+            }
+            
+            print("Fetch failed: \(error?.localizedDescription ?? "Unknown error")")
+        }.resume()
     }
 }
 
